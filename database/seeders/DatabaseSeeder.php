113d076885aa68f8294dd3364ef8c53c40d30bf0
SomnathSen <somnathsen0303@gmail.com>
2026-03-13 03:08:58 +0530

    Fixed DatabaseSeeder class structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,19 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-public function run(): void
+class DatabaseSeeder extends Seeder
 {
-    Role::insert([
-        ['id' => 1, 'name' => 'admin'],
-        ['id' => 2, 'name' => 'teacher'],
-        ['id' => 3, 'name' => 'student'],
-    ]);
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Role::insert([
+            ['id' => 1, 'name' => 'admin'],
+            ['id' => 2, 'name' => 'teacher'],
+            ['id' => 3, 'name' => 'student'],
+        ]);
 
-    User::create([
-        'name' => 'Admin',
-        'email' => 'admin@example.com',
-        'password' => Hash::make('admin123'),
-        'role_id' => 1,
-    ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+            'role_id' => 1,
+        ]);
+    }
 }
